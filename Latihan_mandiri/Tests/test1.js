aacfcdbcfa1958de838f376125e28a7f00c5eba2
Bismvrckz <103980780+Bismvrckz@users.noreply.github.com>
2022-05-25 15:44:15 +0700

    Latihan bang yunan

--- Latihan_mandiri/Tests/test1.js
+++ Latihan_mandiri/Tests/test1.js
@@ -1,2 +1,51 @@
-const q = `kdaweqnjc`;
-console.log(q.length);
+const rawDatas = {
+  nationality: "Canada",
+  owners: ["Linus", "James Hobson", "Mehdi", "Ryan George"],
+  channels: ["Linus Tech Tips", "Hacksmith", "ElectroBoom", "Ryan George"],
+  contents: [
+    "Technology-Review",
+    "Engineering-Invention",
+    "Engineering-Comedy-Education",
+    "Comedy-Sketch",
+  ],
+};
+
+const filterByContent = (keyword, data) => {
+  const rawContents = data.contents;
+  let arr = [];
+  rawContents.forEach((content, ind) => {
+    if (content.toLowerCase().includes(keyword.toLowerCase())) {
+      arr.push(ind);
+    }
+  });
+
+  if (arr.length) {
+    let own = arr.map((x) => {
+      return data.owners[x];
+    });
+
+    let chan = arr.map((x) => {
+      return data.channels[x];
+    });
+
+    let cont = arr.map((x) => {
+      return data.contents[x];
+    });
+
+    let result = {
+      nationality: data.nationality,
+      owners: own,
+      channels: chan,
+      contents: cont,
+    };
+    return result;
+  }
+};
+console.log(filterByContent("comedy", rawDatas));
+
+// example2 = {
+//   nationality: "Canada",
+//   owners: ["Mehdi", "Ryan George"],
+//   channels: ["ElectroBoom", "Ryan George"],
+//   contents: ["Engineering-Comedy-Education", "Comedy-Sketch"],
+// };
