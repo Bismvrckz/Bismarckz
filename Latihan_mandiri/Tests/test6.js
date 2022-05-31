4c84d4f1aacdf2edebd86596d9e5ad3b690ad00b
Bismvrckz <103980780+Bismvrckz@users.noreply.github.com>
2022-05-31 07:19:02 +0700

    codewars

--- Latihan_mandiri/Tests/test6.js
+++ Latihan_mandiri/Tests/test6.js
@@ -1,11 +1,30 @@
-function feast(beast, dish) {
-  if (beast[0] == dish[0] && beast[beast.length - 1] == dish[dish.length - 1]) {
-    return true;
-  } else {
-    return false;
-  }
+function domainName(url) {
+  let urlSplit = url.split("");
+  let target = `.com`;
+  let tagetEnd = [`/`, `.`];
+  let endIdx;
+  urlSplit.forEach((char, idx) => {
+    let i;
+    let charIdx;
+    for (
+      i = 0, charIdx = idx;
+      i < target.length && urlSplit[charIdx] == target[i];
+      i++, charIdx++
+    ) {
+      if (i == target.length - 1) {
+        endIdx = idx;
+      }
+    }
+    if (endIdx) {
+      tagetEnd.forEach((end) => {
+        for (charIdx = endIdx; charIdx > 0; charIdx--) {
+          if (urlSplit[charIdx] == end) {
+            console.log(urlSplit[charIdx]);
+          }
+        }
+      });
+    }
+  });
 }
-// console.log(feast("browr", "bear"));
-
-let bones = "Forgotten Bones";
-console.log(bones.slice(0, bones.length));
+console.log(domainName("http://www.zombie-bites.com"));
+// https://www.codewars.com/kata/514a024011ea4fb54200004b/train/javascript
