3a268cc19bccb8d654b64e37b3c36f48ed190796
Bismvrckz <103980780+Bismvrckz@users.noreply.github.com>
2022-06-16 15:12:37 +0700

    Material UI

--- Project/Arizonna/Client/src/Pages/Welcome/index.js
+++ Project/Arizonna/Client/src/Pages/Welcome/index.js
@@ -1,10 +1,43 @@
 import { Checkbox, Button, Loading } from "@nextui-org/react";
 import { useState, useEffect } from "react";
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import Input from "@mui/material/Input";
+import FilledInput from "@mui/material/FilledInput";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import FormHelperText from "@mui/material/FormHelperText";
+import FormControl from "@mui/material/FormControl";
+import TextField from "@mui/material/TextField";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
 
 export function Home() {
   const [click, setClick] = useState(0);
 
-  // useEffect(setTimeout(afterSignInClick, 2000), [click]);
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  const [values, setValues] = useState({
+    amount: "",
+    password: "",
+    weight: "",
+    weightRange: "",
+    showPassword: false,
+  });
 
   function afterSignInClick() {
     setTimeout(() => {
@@ -23,25 +56,46 @@ export function Home() {
       <p className="text-white text-[5vh] font-[300]">
         <i class="fa-brands fa-atlassian text-cyan-500"></i> Arizonna
       </p>
-      <div className="w-[30%]  rounded-[2vh] opacity-100 bg-[#1E1E1E] font-[montserrat] flex flex-col items-center justify-center h-[50%] ">
+      <div className="w-[30%]  rounded-[2vh] opacity-100 bg-[#1E1E1E] font-[montserrat] flex flex-col items-center justify-center h-[60%] ">
         <div className="flex flex-col justify-center w-[80%]">
           <div className="text-[3vh] mb-[1vh] font-[500] text-white">
             Sign in
           </div>
           <p className="text-[2vh] font-[200] text-white">Login to Arizonna</p>
-          <input
-            placeholder="Email"
-            className="my-[0.5vh] bg-transparent border-solid border-cyan-500 focus:border-teal-500 focus:drop-shadow-[1px_0px_45px_-20px_rgba(93,225,247,0.75)] border-[0.1vh] rounded w-[100%] h-[5vh] text-white"
-          ></input>
-          <input
-            placeholder="Password"
-            type="password"
-            className="my-[0.5vh] bg-transparent border-solid border-cyan-500 focus:border-teal-500 focus:drop-shadow-[1px_0px_45px_-20px_rgba(93,225,247,0.75)] border-[0.1vh] rounded w-[100%] h-[5vh] text-white"
-          ></input>
+          <TextField
+            color="primary"
+            sx={{ m: 0, width: "100%", borderColor: "red" }}
+            id="outlined-basic"
+            label="Email"
+            variant="filled"
+          />
+          <FormControl sx={{ mt: 1, width: "100%" }} variant="filled">
+            <InputLabel htmlFor="filled-adornment-password">
+              Password
+            </InputLabel>
+            <FilledInput
+              id="filled-adornment-password"
+              type={values.showPassword ? "text" : "password"}
+              value={values.password}
+              onChange={handleChange("password")}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+          </FormControl>
           <div className="flex flex-col">
             <Checkbox.Group defaultValue={["Remember_me"]}>
               <Checkbox value="Remember_me" color="gradient">
-                <p className="text-white font-[montserrat] mt-[1.5vh]">
+                <p className="text-white font-[montserrat] my-[3vh]">
                   Remember me
                 </p>
               </Checkbox>
