17ee33bdf67bc7729822c0e2035694271d76bd5e
Bismvrckz <ahmadfayruzsyamil33@gmail.com>
2022-07-14 06:01:44 +0700

    Sql export

---
+++ Project/Arizonna/arizonna.sql
@@ -0,0 +1,58 @@
+-- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
+--
+-- Host: localhost    Database: arizonna
+-- ------------------------------------------------------
+-- Server version	8.0.29
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `user_id` bigint NOT NULL,
+  `username` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `user_password` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `isVerified` tinyint(1) DEFAULT '0',
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1657540099184,'123','ahmadfayruzsyamil33@gmail.com','$2a$10$dmYFgR1UgrdsUijNdEQYLekAsnl1kEp1Y97Xxn6fzSLOL4z1p5Ey2',0,'2022-07-11 18:48:19','2022-07-11 18:48:19'),(1657597301538,'1234','ahmadfayruzs4yamil33@gmail.com','$2a$10$7CvMRtp3J8Xu0.Cx8UZGUOBrwcMdATe9EQvAr/.9kWNVDzOo96PYm',0,'2022-07-12 10:41:41','2022-07-12 10:41:41'),(1657597594571,'123123','123@123.com','$2a$10$1rxBgsi12G7SaAFPIPpaoO33txuKtsiXhsMqOtws.PcsyvYDkLNL2',0,'2022-07-12 10:46:34','2022-07-12 10:46:34'),(1657597713867,'123456','123@456.com','$2a$10$4X38xM47eg7hNYFzHifA4OHt0tFbXIHLwzIPIHGY5ku5sDdKNEdVu',0,'2022-07-12 10:48:33','2022-07-12 10:48:33'),(1657597800558,'123123123','123@sadqwada1313.com','$2a$10$DPn8Wx8QC70d9.4T9dQGAuujMWXFk9MW7xIJ98JI.wpzzdi3BitZa',0,'2022-07-12 10:50:00','2022-07-12 10:50:00');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-07-14  6:01:15
