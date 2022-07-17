3b247808c2cde14fa02ec0ada3bbcebe2680ef4a
Bismvrckz <ahmadfayruzsyamil33@gmail.com>
2022-07-17 17:23:03 +0700

    sql export

--- Project/Arizonna/arizonna.sql
+++ Project/Arizonna/arizonna.sql
@@ -16,6 +16,120 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `comments`
+--
+
+DROP TABLE IF EXISTS `comments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comments` (
+  `comment_id` bigint NOT NULL AUTO_INCREMENT,
+  `user_id` bigint NOT NULL,
+  `post_id` bigint NOT NULL,
+  `commentPhrase` varchar(255) NOT NULL DEFAULT '',
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`comment_id`),
+  KEY `user_id` (`user_id`),
+  KEY `post_id` (`post_id`),
+  CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `posts` (`post_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comments`
+--
+
+LOCK TABLES `comments` WRITE;
+/*!40000 ALTER TABLE `comments` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `likes`
+--
+
+DROP TABLE IF EXISTS `likes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `likes` (
+  `like_id` bigint NOT NULL AUTO_INCREMENT,
+  `user_id` bigint NOT NULL,
+  `post_id` bigint NOT NULL,
+  `liked` tinyint(1) NOT NULL DEFAULT '0',
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`like_id`),
+  KEY `user_id` (`user_id`),
+  KEY `post_id` (`post_id`),
+  CONSTRAINT `likes_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  CONSTRAINT `likes_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `posts` (`post_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `likes`
+--
+
+LOCK TABLES `likes` WRITE;
+/*!40000 ALTER TABLE `likes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `likes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `posts`
+--
+
+DROP TABLE IF EXISTS `posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `posts` (
+  `post_id` bigint NOT NULL AUTO_INCREMENT,
+  `user_id` bigint NOT NULL,
+  `postImage` varchar(100) DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`post_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `posts`
+--
+
+LOCK TABLES `posts` WRITE;
+/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sequelizemeta`
+--
+
+DROP TABLE IF EXISTS `sequelizemeta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sequelizemeta` (
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`name`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sequelizemeta`
+--
+
+LOCK TABLES `sequelizemeta` WRITE;
+/*!40000 ALTER TABLE `sequelizemeta` DISABLE KEYS */;
+INSERT INTO `sequelizemeta` VALUES ('20220714104111-create-users.js'),('20220716075716-create-post.js'),('20220716083530-create-comment.js'),('20220716083547-create-like.js');
+/*!40000 ALTER TABLE `sequelizemeta` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `users`
 --
 
@@ -23,17 +137,20 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
-  `user_id` bigint NOT NULL,
-  `username` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `user_password` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `user_id` bigint NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `bio` varchar(255) NOT NULL,
+  `fullname` varchar(255) DEFAULT '',
+  `user_avatar` varchar(255) DEFAULT 'C:/Users/Wicked Wench/Documents/GitHub/GeneralRepo/Project/Arizonna/Api/public/userAvatar/default-avatar.png',
+  `user_password` varchar(100) NOT NULL,
   `isVerified` tinyint(1) DEFAULT '0',
-  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1658020911882 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -42,7 +159,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1657540099184,'123','ahmadfayruzsyamil33@gmail.com','$2a$10$dmYFgR1UgrdsUijNdEQYLekAsnl1kEp1Y97Xxn6fzSLOL4z1p5Ey2',0,'2022-07-11 18:48:19','2022-07-11 18:48:19'),(1657597301538,'1234','ahmadfayruzs4yamil33@gmail.com','$2a$10$7CvMRtp3J8Xu0.Cx8UZGUOBrwcMdATe9EQvAr/.9kWNVDzOo96PYm',0,'2022-07-12 10:41:41','2022-07-12 10:41:41'),(1657597594571,'123123','123@123.com','$2a$10$1rxBgsi12G7SaAFPIPpaoO33txuKtsiXhsMqOtws.PcsyvYDkLNL2',0,'2022-07-12 10:46:34','2022-07-12 10:46:34'),(1657597713867,'123456','123@456.com','$2a$10$4X38xM47eg7hNYFzHifA4OHt0tFbXIHLwzIPIHGY5ku5sDdKNEdVu',0,'2022-07-12 10:48:33','2022-07-12 10:48:33'),(1657597800558,'123123123','123@sadqwada1313.com','$2a$10$DPn8Wx8QC70d9.4T9dQGAuujMWXFk9MW7xIJ98JI.wpzzdi3BitZa',0,'2022-07-12 10:50:00','2022-07-12 10:50:00');
+INSERT INTO `users` VALUES (1,'abcdef','mail@mdefail.com','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam neque quam, interdum at nulla ut, ultrices vehicula dui. In diam diam, vestibulum eu iaculis pretium, congue eu nisl.','','https://cdn.myportfolio.com/5f38493d38f9b59d0dd58e3b207d3992/6e3b98ff-29bf-4d70-8c9c-7aadf934cfa7_rwc_0x0x622x623x622.jpg?h=5566f629d1f26ec29d9aa11f9bc42c21','123',0,'2022-07-16 11:38:29','2022-07-17 16:51:00'),(1657973845517,'Bismvrckz','Bismarck@R3ich.com','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam neque quam, interdum at nulla ut, ultrices vehicula dui. In diam diam, vestibulum eu iaculis pretium, congue eu nisl.','Otto von','https://cdn.myportfolio.com/5f38493d38f9b59d0dd58e3b207d3992/6e3b98ff-29bf-4d70-8c9c-7aadf934cfa7_rwc_0x0x622x623x622.jpg?h=5566f629d1f26ec29d9aa11f9bc42c21','$2a$10$E0nEEiv6OKOcyFTVQ96Wp.2x/CkydsMH/xWAX3NbWhH8Q5CBOu2d6',1,'2022-07-16 12:17:25','2022-07-17 16:51:00'),(1658020911881,'07M@r200412430','123@mail.com','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam neque quam, interdum at nulla ut, ultrices vehicula dui. In diam diam, vestibulum eu iaculis pretium, congue eu nisl.','','https://cdn.myportfolio.com/5f38493d38f9b59d0dd58e3b207d3992/6e3b98ff-29bf-4d70-8c9c-7aadf934cfa7_rwc_0x0x622x623x622.jpg?h=5566f629d1f26ec29d9aa11f9bc42c21','$2a$10$/5aXe7bp4PnwUNQHPK7cIeCIHik1Ce90lwsQJ6dOIG5ImJAongJ5C',1,'2022-07-17 01:21:51','2022-07-17 16:51:00');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -55,4 +172,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-07-14  6:01:15
+-- Dump completed on 2022-07-17 17:22:41
