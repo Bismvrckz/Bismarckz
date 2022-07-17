643e43b28c58e4681239cb991bb5b55419b85798
Bismvrckz <103980780+Bismvrckz@users.noreply.github.com>
2022-07-17 19:39:32 +0700

    Edit Profile

--- Project/Arizonna/Client/pages/editProfile.js
+++ Project/Arizonna/Client/pages/editProfile.js
@@ -1,6 +1,8 @@
 import { getSession } from "next-auth/react";
 import React from "react";
 import axiosInstance from "../services/axiosinstance";
+import TextField from "@mui/material/TextField";
+import { Button } from "@mui/material";
 
 function editProfile(props) {
   const { bio, username, createdAt, isVerified, fullname, email, user_avatar } =
@@ -8,9 +10,46 @@ function editProfile(props) {
 
   return (
     <div className="bg-gradient-to-r flex items-center justify-center from-teal-900 to-cyan-900 w-[100vw] h-[100vh]">
-      <div className="w-[95%] h-[95%]">
-        <img className="rounded-[50%] w-[10vw] h-[10vw]" src={user_avatar} />
-        {bio}
+      <div className="w-[95%] h-[95%] flex flex-col justify-evenly items-start">
+        <div className="flex items-center">
+          <img className="rounded-[50%] w-[10vw] h-[10vw]" src={user_avatar} />
+          <label
+            for="imageInput"
+            className="font-[montserrat] hover:bg-cyan-500 ml-[3vw] flex items-center justify-center bg-cyan-800 h-[5vh] w-[10vw] rounded-[3vh]"
+          >
+            <a className="cursor-pointer">Change Image</a>
+            <input id="imageInput" className="hidden" type={"file"} />
+          </label>
+
+          {/* <Button className="ml-[3vw] w-[14vw]" variant="outlined">
+            Change Picture
+          </Button> */}
+        </div>
+        <TextField
+          className="w-[20vw]"
+          label="Username"
+          variant="outlined"
+          value={username}
+          autoComplete="off"
+          color="info"
+        />
+        <TextField
+          className="w-[20vw]"
+          label="Fullname"
+          variant="outlined"
+          value={fullname}
+          autoComplete="off"
+          color="info"
+        />
+        <TextField
+          className="w-[50vw]"
+          label="Bio"
+          variant="outlined"
+          value={bio}
+          autoComplete="off"
+          color="info"
+        />
+        <Button variant="contained">Save Changes</Button>
       </div>
     </div>
   );
